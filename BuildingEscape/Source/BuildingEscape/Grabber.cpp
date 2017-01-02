e7debce6b73c5bbe239989b04f02c8f5f7bdbf6a
DESKTOP-VKNOI85\Casper <denis.coccodi@gmail.com>
2017-01-02 18:27:44 +0100

    BE13 Added a line starting from the pawn and telling the console what it's hitting. Filters for Physics Bodies only (Iitems with physics checked and weight set)

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -30,19 +30,52 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// Get player(pawn) viewpoint this tick
+	/// Get player(pawn) viewpoint this tick
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
 		OUT PlayerViewPointLocation, //The word OUT was defined at the top to do nothing, 
 		OUT PlayerViewPointRotation  //just to remind us that the parameters are passed by reference
 	);
-	//Log out to test 
-	UE_LOG(LogTemp, Warning, TEXT("The grabber is watching from %s towards %s"), 
+	///Log out to test 
+	/*UE_LOG(LogTemp, Warning, TEXT("The grabber is watching from %s towards %s"), 
 		*PlayerViewPointLocation.ToString(),
 		*PlayerViewPointRotation.ToString()
+	);*/
+
+	///When a rotator returns a vector, it will be as if the position would be one unit away from the point of origin.
+	FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotation.Vector()*Reach);
+	///Draw a red trace in the world to visualize
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd, 
+		FColor(255,0,0),
+		false,///we don't want the line to persist over time. The line is redrawn every frame.
+		0.f, ///Duration of the line in case we wanted it to stay up after the frame.
+		0.f,
+		5.f
 	);
-	//ray-cast out to reach distance (imagine Cyclop laser ray)
+	///Setup query parameters.We want a simple trace, not a heavy complex one with all the details, hence the false.
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());///we ignore ourselves because the ray-cast starts in our character and would always be colliding
+
+	///Line Trace (ray-cast) out to reach distance (imagine Cyclop laser ray)
+	FHitResult LineTraceHit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT LineTraceHit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+
+	AActor* ActorHit = LineTraceHit.GetActor();
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("I've just hit the %s Physics Body!"),
+			*(ActorHit->GetName())
+		);
+	}
 	//see what we hit
 }
 
